0f61d548c1f3a6470c9749e1397be17322b31490
nickratt <nickrattanachane@gmail.com>
2019-05-23 13:32:48 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,130 @@
+float balance = 0;
+float saving = 0;
+
+#include <iostream>
+using namespace std;
+
+
+void menu(){
+  cout << "***WELCOME TO THE ONLINE BANKING SYSTEM***" << endl;
+  cout << "Current balance: " << balance << endl;
+  cout << "Current savings: " <<saving << endl; 
+  cout << "     " << endl;
+  cout << "What would you like to do in this session?" << endl;
+
+}
+
+void options()
+{
+  cout << "01. Deposit" << endl;
+  cout << "02. withdrawal" << endl;
+  cout << "03. Transfer" << endl;
+  cout << "Enter an option: " << flush;
+}
+
+
+int deposit(int x)
+{
+  int deposit = x;
+  balance = balance + deposit;
+  return balance;
+}
+
+int withdraw(int x){
+  int withdraw = x;
+  balance = balance - withdraw;
+  return balance;
+}
+
+int transfer(int x){
+  char ans;
+  cout << "Would you like to transfer from your savings? y/n" << endl;
+  cin >> ans;
+  if (ans == 'n'){
+    cout << "How much would you like to take out of your checkings to your savings?: " << flush;
+    cin >> x;
+    balance = balance - x;
+    saving = saving + x;
+    
+    return saving;
+    
+  }else{
+    cout << "How much would you like to take from your savings to checking : " << flush;
+    cin >> x;
+    saving = saving - x;
+    balance = balance + x;
+
+    return balance;
+
+  }
+
+
+}
+
+void overdraft(){
+  int newbalance;
+  int newsaving;
+  if (balance < 0){
+    cout << "OVERDRAFT FEE: 25%!!!" << endl;
+    newbalance = balance - (.25);
+    cout << "Your balance is now: " << newbalance << endl;
+
+
+  }else if(saving < 0) {
+    cout << "OVERDRAFT FEE: 25%!!!" << endl;
+    newsaving = saving - .25;
+    cout << "Your saving is now: " << newsaving<< endl;
+  }
+  else{
+    cout << "No overdraft fee" << endl;
+  }
+}
+
+
+int main()
+{
+  int ans;
+  int addMoney;
+  int takeout;
+  int trans;
+
+  menu();
+  options();
+  cout << ans << flush;
+  cin >> ans;
+  
+  
+
+  switch (ans){
+    case 1:
+    cout << "How much would you like to deposit: " << flush;
+    cin >> addMoney;
+    deposit(addMoney);
+    cout << "Your balance is now: $" << balance << endl;
+    break;
+
+    case 2:
+    cout << "How much would you like to withdraw: " << flush;
+    cin >> takeout;
+    withdraw(takeout);
+    cout << "Your balance is now: $" << balance << endl;
+    break;
+
+    case 3:
+    cout << "How much would you like to transfer: " << flush;
+    transfer(trans);
+    cout << "Your balance is now: $" << balance << endl; 
+    cout << "Your saving is now: $" << saving << endl;
+    break;
+
+    default:
+    cout << "Not a valid option" << endl;
+    break;
+
+  
+  }
+  overdraft();
+
+} 
+
+
